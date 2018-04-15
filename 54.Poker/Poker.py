1f88acc59c1a06b907ceb78fd09ede90f3792ed7
miskoralgol23 <31490362+miskoralgol23@users.noreply.github.com>
2018-04-15 22:54:14 +0200

    Wywalenie tekstu, troche ogarniecie kodu.

--- 54.Poker/Poker.py
+++ 54.Poker/Poker.py
@@ -1,45 +1,20 @@
-"""
-In the card game poker, a hand consists of five cards and are ranked, from lowest to highest, in the following way:
 
-High Card: Highest value card.
-One Pair: Two cards of the same value.
-Two Pairs: Two different pairs.
-Three of a Kind: Three cards of the same value.
-Straight: All cards are consecutive values.
-Flush: All cards of the same suit.
-Full House: Three of a kind and a pair.
-Four of a Kind: Four cards of the same value.
-Straight Flush: All cards are consecutive values of same suit.
-Royal Flush: Ten, Jack, Queen, King, Ace, in same suit.
-
-The cards are valued in the order:
-2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King, Ace.
-
-HC - High Card
-OP - One Pair
-TP - Two Pairs
-Th - Three of a Kind
-S - Straight
-F - Flush
-FH - Full House
-Fo - Four of a Kind
-SF - Straight Flush
-RF - Royal Flush
-"""
-
-c_dict = {"J":"Jack","Q":"Queen","K":"King","A":"Ace"}
 hands_order = ["HC","OP","TP","Th", "S", "F", "FH", "Fo", "SF", "RF"]
 
+
+# Loading the data from the source file
 with open("p054_poker.txt") as f:
     dat=f.readlines()
 
 dat = [d.replace("\n","") for d in dat]
 
-def game_no(n):
-    return dat[n]
-
 card_suits = ["C","D","H","S"]
 card_numbers = [str(x+2) for x in range(8)]
+for num in ["T", "J","Q","K","A"]:
+    card_numbers.append(num)
+
+deck =  [num + suit for num in card_numbers for suit in card_suits]
+
 
 def card_value(cardNo):
     return card_numbers.index(cardNo) + 2
@@ -47,14 +22,11 @@ def card_value(cardNo):
 def suit_value(suitLetter):
     return card_suits.index(suitLetter)
 
+
 def hand_no(n,player=1):
-    temp = game_no(n)[0:14].split() if player==1 else game_no(n)[15:].split()
+    temp = dat[n][0:14].split() if player==1 else dat[n][15:].split()
     return sorted(temp, key=lambda x: card_value(x[0]) - float(suit_value(x[1]))/4,reverse=True)
 
-for num in ["T", "J","Q","K","A"]:
-    card_numbers.append(num)
-
-deck =  [num + suit for num in card_numbers for suit in card_suits]
 
 from collections import Counter
 
@@ -63,9 +35,6 @@ def cnts(hand):
     s = Counter(map(lambda x: x[1], hand))
     return (n,s)
 
-h = hand_no(9)
-print(h)
-x = cnts(h)
 
 if len(x[0]) < 5:
     if len(x[0]) == 4:
