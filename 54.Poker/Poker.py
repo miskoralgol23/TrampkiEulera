c1c3ea73c4a60cf1f88fd7ab28f9d463ad2eb0aa
miskoralgol23 <31490362+miskoralgol23@users.noreply.github.com>
2018-05-20 00:00:30 +0200

    Tym oto programem uzyskalem naraszcie poprawny wynik!!!!

--- 54.Poker/Poker.py
+++ 54.Poker/Poker.py
@@ -1,52 +1,60 @@
+from collections import Counter
+import os
 import pdb
-hands_order = ["HC","OP","TP","Th", "S", "F", "FH", "Fo", "SF"]
-#no RF really needed as it's just SF
+
+hands_order = ["HC", "OP", "TP", "Th", "F", "S", "FH", "Fo", "SF"]
+# no RF really needed as it's just SF
+
+
+print(os.getcwd())
+
 
 # Loading the data from the source file
-#############################################
+# ############################################
 with open("p054_poker.txt") as f:
-    dat=f.readlines()
+    dat = f.readlines()
 
-dat = [d.replace("\n","") for d in dat]
+dat = [d.replace("\n", "") for d in dat]
 
-card_suits = ["C","D","H","S"]
+card_suits = ["C", "D", "H", "S"]
 card_numbers = [str(x+2) for x in range(8)]
-for num in ["T", "J","Q","K","A"]:
+
+for num in ["T", "J", "Q", "K", "A"]:
     card_numbers.append(num)
 
-deck =  [num + suit for num in card_numbers for suit in card_suits]
+deck = [num + suit for num in card_numbers for suit in card_suits]
 #############################################
 
-def card_value(cardNo):
-    return card_numbers.index(cardNo) + 2
 
-def suit_value(suitLetter):
-    return card_suits.index(suitLetter)
-
-def hand_no(n,player=1):
-    return dat[n][0:14].split() if player==1 else dat[n][15:].split()
+def card_value(card_number):
+    return card_numbers.index(card_number) + 2
 
 
+def suit_value(suit_letter):
+    return card_suits.index(suit_letter)
 
-def hands_no(n,player=1):
-    return [dat[n][0:14].split(),dat[n][15:].split()]
+
+def hand_no(n, player=1):
+    return dat[n][0:14].split() if player == 1 else dat[n][15:].split()
 
 
-from collections import Counter
+def hands_no(n, player=1):
+    return [dat[n][0:14].split(), dat[n][15:].split()]
+
 
 def cnts(hand):
     n = Counter(map(lambda x: x[0], hand))
     s = Counter(map(lambda x: x[1], hand))
-    return (n,s)
+    return (n, s)
 
-def isStraight(cards):
+def is_straight(cards):
     cards=[card_value(x[0]) for x in cards]
     cards=sorted(cards)
     cards=[c - cards[0] for c in cards]
-    if cards==[0,1,2,3,4]:
-        return(True)
+    if cards == [0,1,2,3,4]:
+        return True
 
-def hand_conf(cards):
+def hand_configuration(cards):
     interim_result="HC"
     x=cnts(cards)
     if len(x[1])==1:
@@ -65,50 +73,67 @@ def hand_conf(cards):
         if multiple==[2,2]:
             interim_result="TP"
     else:
-        if isStraight(cards):
+        if is_straight(cards):
             if interim_result=="F":
                 interim_result="SF"
             else:
                 interim_result="S"
     return hands_order.index(interim_result)
 
-def hand_val(cards):
+def hand_value(cards):
     num_cnt=cnts(cards)[0]
-    cards=[(15**card_value(x[0]))**(num_cnt[x[0]]) for x in cards]
-    return(cards)
+    cards=[((15*card_value(x[0]))**card_value(x[0]))**(num_cnt[x[0]]) for x in cards]
+    return cards
 
-def hand_vals(n):
-    h1=hand_no(n)
-    h2=hand_no(n,2)
-    hv1=hand_val(h1)
-    hv2=hand_val(h2)
-    hv1v2=list(hv1)
-    hv1v2.extend(hv2)
-    m=max(hv1v2)
-    return m
+def has_player1_higher_value(game_no):
+    player1_hand = hand_no(game_no, 1)
+    player2_hand = hand_no(game_no, 2)
+    player1_values = cards_values_in_hand(player1_hand)
+    player2_values = cards_values_in_hand(player2_hand)
+    p1_max = player1_values.pop()
+    p2_max = player2_values.pop()
+    while p1_max == p2_max:
+        p1_max = player1_values.pop()
+        p2_max = player2_values.pop()
+    return p1_max > p2_max
+
+def cards_values_in_hand(cards):
+    cards = [card_value(x[0]) for x in cards]
+    c = Counter(cards)
+    cards = sorted([card_val*(15**c[card_val]) for card_val in cards])
+    return cards
+
 
 def run_some_test():
     for x in range(4):
         tst=hand_no(x)
         tzt=hand_no(x,2)
-        conf=hand_conf(tst)
-        conf2=hand_conf(tzt)
-        print(str(tst) + "  "+ str(x) + " "+ str(conf) + " Val " + str(hand_val(tst)) +"="+ str(sum(hand_val(tst))))
-        print(str(tzt) + "  "+ str(x) + " "+ str(conf2) + " Val " + str(hand_val(tzt)) + "=" + str(sum(hand_val(tzt))))
-        print( "1" if sum(hand_val(tst)) > sum(hand_val(tzt)) else "2")
+        conf=hand_configuration(tst)
+        conf2=hand_configuration(tzt)
+        print(str(tst) + "  " + str(x) + " " + str(conf) + " Val " + str(hand_value(tst)) + "=" + str(sum(hand_value(tst))))
+        print(str(tzt) + "  " + str(x) + " " + str(conf2) + " Val " + str(hand_value(tzt)) + "=" + str(sum(hand_value(tzt))))
+        print( "1" if sum(hand_value(tst)) > sum(hand_value(tzt)) else "2")
 
-run_some_test()
-print(hand_vals(2))
 
-wins=[]
-for x in range(1000):
-    if hand_conf(hand_no(x)) > hand_conf(hand_no(x,2)):
-        wins.append(x)
-    else:
-        if hand_conf(hand_no(x)) ==hand_conf(hand_no(x,2)) and sum(hand_val(hand_no(x))) > sum(hand_val(hand_no(x,2))):
-            print(str(x) + " " + str(hand_conf(hand_no(x))))
+if __name__ == "__main__":
+    wins=[]
+    for x in range(1000):
+        if hand_configuration(hand_no(x)) > hand_configuration(hand_no(x, 2)):
             wins.append(x)
+        else:
+            if hand_configuration(hand_no(x)) == hand_configuration(hand_no(x, 2)) and has_player1_higher_value(x):
+            # if hand_configuration(hand_no(x)) == hand_configuration(hand_no(x, 2)) and sum(hand_value(hand_no(x))) > sum(hand_value(hand_no(x, 2))):
+                print(str(x) + " " + str(hand_configuration(hand_no(x))))
+                wins.append(x)
+    print("Number of wins:", len(wins))
+
 
 def both(n):
-    print(str(hand_no(n)) + "  " + str(hand_conf(hand_no(n))))
-    print(str(hand_no(n,2)) + "  " + str(hand_conf(hand_no(n,2))))
+    print(str(hand_no(n)) + "  " + str(hand_configuration(hand_no(n))))
+    print(str(hand_no(n,2)) + "  " + str(hand_configuration(hand_no(n, 2))))
+
+#
+# for x in range(100):
+#     print(x, hand_no(x), hand_no(x,2), has_player1_higher_value(x))
+#     if sum(hand_value(hand_no(x))) > sum(hand_value(hand_no(x, 2))) and not has_player1_higher_value(x):
+#         print "REEEEEEEEEEEEEEEe"
